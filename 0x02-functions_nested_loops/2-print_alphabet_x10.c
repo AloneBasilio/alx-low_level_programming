e05ce4a723d8b2eb428971ccfcb3fd19b6e0f013
Alone Basilio <alonnyduwart9@gmail.com>
2023-08-16 21:06:13 +0200

    x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/*
+ * print_alphabet - print the alphabet
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	int i,j;
+
+	i = 0;
+
+	while(i < 10)
+	{
+		for( j = 97; j < 123; j++)
+			putchar(j);
+		putchar('\n');
+		i++;
+	}
+}
