2a75f44a3733853df7e9b03445af22ddb2b71646
Alone Basilio <alonnyduwart9@gmail.com>
2023-08-16 21:35:38 +0200

    Is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/*
+ * int  _islower (int c) - 
+ * paramaters c - int
+ * Return 1 or 0
+ */
+int _islower(int c)
+{
+	if(c > 96 && c < 123)
+		return (1);
+	else 
+		return (0);
+}
