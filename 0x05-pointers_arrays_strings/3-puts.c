5e5fb055fae13ca9d03b4e85e52fa3f78213a699
Alone Basilio <alonnyduwart9@gmail.com>
2023-08-22 23:18:16 +0200

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _puts - prints a string
+ * @str: is a string
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
