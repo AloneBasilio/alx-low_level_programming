2fece5aa1ad5e879c797e0effae8ab45bcbff495
Alone Basilio <alonnyduwart9@gmail.com>
2023-08-24 22:37:32 +0200

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -6,6 +6,7 @@
  * _strncat - concatenates two strings
  * @dest: destine's string
  * @src: source's string
+ * @n: lenght of string
  * Return: string
  */
 char *_strncat(char *dest, char *src, int n)
