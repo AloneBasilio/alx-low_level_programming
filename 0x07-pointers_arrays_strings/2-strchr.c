a96a3e115a3d6d1cafa904ed89623ebd4e0e7a4e
Alone Basilio <alonnyduwart9@gmail.com>
2023-08-28 22:08:01 +0200

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -6,31 +6,18 @@
  * _strchr - locates a character in a string
  * @s: string
  * @c: char
+ * @'\0': character null
  *
  * Return: string
  */
 char *_strchr(char *s, char c)
 {
-/**
- * 	unsigned int n;
- *	unsigned int i;
- *	char *r;
- *
- *	n = strlen(s);
- *	for (i = 0; i < n; i++)
- *	{
- *		if (s[i] != c)
- *			continue;
- *		r+=s[i];
- *	}
- *	if (r != '')
- */		
 	while (*s != '\0')
 	{
 		if (*s == c)
 			return (s);
 		s++;
-        }
+	}
 	if (*s == c)
 		return (s);
 	return (NULL);
