5fc492e50fa5717e121ce4e39fea78e7aa2d753a
Alone Basilio <alonnyduwart9@gmail.com>
2023-09-02 00:27:33 +0200

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - print the number of arguments passed into it
+ * @argc: length of arguments
+ * @argv: the arguments
+ *
+ * Return: (0)
+ */
+int main(int argc, char *argv[])
+{
+	if (strlen(*argv) > 0)
+		printf("%d\n",(argc - 1));
+	return (0);
+}
